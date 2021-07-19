e6e1f3041c99b505d0518e70201228cfaf48b9f9
bschweitzer326 <bret.schweitzer16@gmail.com>
2021-07-19 17:35:01 -0400

    login & registration

---
+++ src/main/java/com/bs/loginregistration/validators/UserValidator.java
@@ -0,0 +1,28 @@
+package com.bs.loginregistration.validators;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.bs.loginregistration.models.User;
+
+@Component
+public class UserValidator implements Validator {
+    
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+    
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user = (User) target;
+        
+        if (!user.getPasswordConfirmation().equals(user.getPassword())) {
+            
+            errors.rejectValue("passwordConfirmation", "Match");
+        }         
+    }
+}
+
+
